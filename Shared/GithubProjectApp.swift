af4bd57cc3eadd4b302bc4bceef7ef7b8749a75b
ManmeetkGill <mkg38780@uga.edu>
2021-06-08 14:42:40 -0400

    Initial Commit

---
+++ Shared/GithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubProjectApp.swift
+//  Shared
+//
+//  Created by manmeet on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
